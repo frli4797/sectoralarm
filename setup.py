2911baa1e6a240559c697a611da997b9c2a68b2e
Mikael Schultz <mikael@dofiloop.com>
2017-09-17 18:50:54 +0200

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='Sector Alarm',
+    version='0.1',
+    packages=[''],
+    url='https://github.com/MikaelSchultz/sectoralarm',
+    license='MIT License',
+    author='Mikael Schultz',
+    author_email='mikael@dofiloop.com',
+    description='A simple library for Sector Alarm.'
+)
