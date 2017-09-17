284f63cafeaea199a0b369846c38a55f94387da6
Mikael Schultz <mikael@dofiloop.com>
2017-09-17 18:37:06 +0200

    Initial commit

---
+++ hello.py
@@ -0,0 +1,17 @@
+# Define a function
+def world():
+    print("Hello, World!")
+
+# Define a variable
+shark = "Sammy"
+
+
+# Define a class
+class Octopus:
+    def __init__(self, name, color):
+        self.color = color
+        self.name = name
+
+    def tell_me_about_the_octopus(self):
+        print("This octopus is " + self.color + ".")
+        print(self.name + " is the octopus's name.")
